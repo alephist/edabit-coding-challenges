c9ca60ddc77430793d9adabc44faa231e6a124f0
Justin Necesito <justin.necesito@gmail.com>
2020-11-22 13:04:53 +0800

    Added description and link to Equal Numbers challenge in JS folder

--- js/equalNumbers.js
+++ js/equalNumbers.js
@@ -1,3 +1,6 @@
+// Create a function that takes two integers and checks if they are equal.
+// https://edabit.com/challenge/Tbvjwh5GKRbxd3vyD
+
 const isEqual = (num1, num2) => {
   if (typeof num1 !== "number" || typeof num2 !== "number") return false;
 

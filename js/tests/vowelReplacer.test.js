aedcfdeb2d94f0c52ff75ec1c5e02ead257ef513
Justin Necesito <justin.necesito@gmail.com>
2020-12-15 16:09:27 +0800

    Fixed one test input in vowelReplacer.test.js

--- js/tests/vowelReplacer.test.js
+++ js/tests/vowelReplacer.test.js
@@ -1,7 +1,7 @@
 const replaceVowels = require("../vowelReplacer");
 
 const data = [
-  ["the aardletk", "#", "th# ##rdv#rk"],
+  ["the aardvark", "#", "th# ##rdv#rk"],
   ["minnie mouse", "?", "m?nn?? m??s?"],
   ["shakespeare", "*", "sh*k*sp**r*"],
   ["all is fair in love and war", "<", "<ll <s f<<r <n l<v< <nd w<r"]

946d64ce9e7c87016c412cc33188fe1a90a751f1
Justin Necesito <justin.necesito@gmail.com>
2021-08-19 13:30:03 +0800

    Added Convert Number to String of Dashes challenge in Python folder

---
+++ python/number_to_dashes.py
@@ -0,0 +1,11 @@
+"""
+Convert Number to String of Dashes
+
+Create a function that takes a number (from 1 - 60) and returns a corresponding string of hyphens.
+
+https://edabit.com/challenge/f3jm7sk7LaYttYyLP
+"""
+
+
+def num_to_dashes(num: int) -> str:
+    return "-" * num
